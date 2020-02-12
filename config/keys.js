bd75a4361971c6694c1782662fe9dcd6a56e762f
hemant <nhemnt@gmail.com>
2020-02-13 00:45:30 +0530

    make object values using string to pass the build on heroku

--- config/keys.js
+++ config/keys.js
@@ -7,14 +7,14 @@ module.exports = {
   secretOrKey: process.env.secretOrKey || config.secretOrKey,
   mapBoxKey: process.env.mapBoxKey || config.mapBoxKey,
   arcgis: {
-    clientId: process.env["arcgis.clientId"] || config.arcgis.clientId,
-    clientSecret: process.env["arcgis.clientSecret"] || config.arcgis.clientSecret,
-    authUrl: process.env["arcgis.authUrl"] || config.arcgis.authUrl,
-    geocodeEndpoint: process.env["arcgis.geocodeEndpoint"] || config.arcgis.geocodeEndpoint,
-    reverseGeocodeEndpoint: process.env["arcgis.reverseGeocodeEndpoint"] || config.arcgis.reverseGeocodeEndpoint,
-    expiry: process.env["arcgis.expiry"] || config.arcgis.expiry
+    clientId: process.env["arcgis.clientId"] || config.arcgis["clientId"],
+    clientSecret: process.env["arcgis.clientSecret"] || config.arcgis["clientSecret"],
+    authUrl: process.env["arcgis.authUrl"] || config.arcgis["authUrl"],
+    geocodeEndpoint: process.env["arcgis.geocodeEndpoint"] || config.arcgis["geocodeEndpoint"],
+    reverseGeocodeEndpoint: process.env["arcgis.reverseGeocodeEndpoint"] || config.arcgis["reverseGeocodeEndpoint"],
+    expiry: process.env["arcgis.expiry"] || config.arcgis["expiry"]
   },
   google: {
-    key:  process.env["google.key"] || config.google.key
+    key:  process.env["google.key"] || config.google["key"]
   }
 };
